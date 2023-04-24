9afbfdf952e0e953b26b6d0b32ec31588087a70f
RoseeyaT <124545950+RoseeyaT@users.noreply.github.com>
2023-04-24 11:33:29 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint ={
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "สนามบินสุวรรณภูมิ",
+        "level": 10,
+        "type": "Special"
+      },
+      "geometry": {
+        "coordinates": [
+          100.7506235256509,
+          13.684278149512807
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เกาะสมุย",
+        "level": 9,
+        "type": "Nutural"
+      },
+      "geometry": {
+        "coordinates": [
+          100.02913339623564,
+          9.529158770875341
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "วัดอุโมงค์",
+        "level": 8,
+        "type": "Culture"
+      },
+      "geometry": {
+        "coordinates": [
+          98.95272099484453,
+          18.78348205682458
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "เกาะกูด",
+        "level": 7,
+        "type": "Nutural"
+      },
+      "geometry": {
+        "coordinates": [
+          102.56700881200794,
+          11.655835616381466
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "พัทยา",
+        "level": 8,
+        "type": "Nutural"
+      },
+      "geometry": {
+        "coordinates": [
+          100.88604950687085,
+          12.935413002896752
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "สวนสนุกดรีมเวิลด์",
+        "level": 8,
+        "type": "Special"
+      },
+      "geometry": {
+        "coordinates": [
+          100.67531281460128,
+          13.991824707615024
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "ถ้ำมรกต",
+        "level": 8,
+        "type": "Nutural"
+      },
+      "geometry": {
+        "coordinates": [
+          99.28373287382499,
+          7.3692781719621365
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "อุทยานประวัติศาสตร์พระนครศรีอยุธยา",
+        "level": 8,
+        "type": "Culture"
+      },
+      "geometry": {
+        "coordinates": [
+          100.57168809008272,
+          14.352506194791204
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "อาสนวิหารพระนางมารีอาปฏิสนธินิรมล",
+        "level": 8,
+        "type": "Culture"
+      },
+      "geometry": {
+        "coordinates": [
+          102.11856550000198,
+          12.609134906176195
+        ],
+        "type": "Point"
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name": "มัสยิดต้นสน",
+        "level": 8,
+        "type": "Culture"
+      },
+      "geometry": {
+        "coordinates": [
+          100.4886877711669,
+          13.740034759455924
+        ],
+        "type": "Point"
+      }
+    }
+  ]
+}
